63f24e8fe4161396e5f0d5c5bb64b5b750c964b4
JRudransh <JRudransh@protonmail.com>
2022-01-13 09:38:31 +0530

    JS Class

---
+++ main.js
@@ -0,0 +1,91 @@
+// It will output hello js in console
+// console.log('Hello JS')
+
+// Basic syntex
+// let x, y, z; // Variable
+// This is a comment
+// var x = 10;
+
+/*
+This is multi 
+line comment
+*/
+
+// Veriable or identifires
+// Case sensitive
+// Must begin with letters
+// Also begin with _ $
+// Name can contain letter, digits, _, $
+
+// let x2 = 5;
+// let $X = 200;
+// var _y = 10;
+
+// const price = 20;
+// console.log(price);
+
+// Datatype
+// 1. Primitive data type
+//  String
+//  Number
+//  Boolean
+//  Undefined
+//  Null
+
+// 2. Non-primitive data type, (Reference)
+// Object
+// Array
+
+// String
+// let x = 'Hello';
+// let y = "JS";
+// let z = `HELLO JS`;
+
+// Numbers
+// let x = 1; // int
+// let y = 1.2 // float
+// let z = 0x14; // Hex
+
+
+// Boolean
+// x = true
+// y = false
+
+// Undefined
+// let x;
+
+// Null
+// let x = 'Hello';
+// x = null;
+
+
+// JS Operators
+
+// Arithmatic
+// +
+// -
+// *
+// **  // Power
+// /
+// %  // Reminder
+// ++ // Increse value by 1
+// -- // Decrese  value by 1
+
+// Assignment
+// =    | x = 10
+// +=   | x += 2    |   x = x + 2
+// -=   | x -= 2    |   x = x - 2
+// *=   | x *= 2    |   x = x * 2
+// /=   | x /= 2    |   x = x / 2
+// %=   | x %= 3    |   x = x % 3
+// **=  | x **= 2   |   x = x ** 2
+
+// Comparision
+// ==       equal to
+// ===      equal value and equal type
+// !=       not equal to
+// !==      not equal value or not equal type
+// >        grater than
+// <        less than
+// >=       grater than or equal to
+// <=       less than or equal to
